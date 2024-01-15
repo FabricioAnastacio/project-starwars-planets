340c4e3e20231467ae21bae093e3662989304b45
Fabricio Rodrigues <fabricio12nastacio@gmail.com>
2024-01-15 17:54:35 -0300

    Adiciona a quebra de linha dentro da tabela e finaliza algumas estilizações

--- src/components/Table.js
+++ src/components/Table.js
@@ -191,7 +191,7 @@ export default function TablePlanets() {
                 <td>{ planet.terrain }</td>
                 <td>{ planet.surface_water }</td>
                 <td>{ planet.population }</td>
-                <td>{ planet.films }</td>
+                <td>{ planet.films.join(' ') }</td>
                 <td>{ planet.created }</td>
                 <td>{ planet.edited }</td>
                 <td>{ planet.url }</td>
